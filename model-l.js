f84d41fbb5d7b66d2980691ce4a4c46907a9077b
Richard J Rosa-Serrano <richardjrosaserrano@gmail.com>
2021-07-26 22:19:52 -0400

    Add a variable assigned to array

--- model-l.js
+++ model-l.js
@@ -5,3 +5,4 @@ var number = 1;
 var programTries = 2;
 var isMotivated = true;
 var isTired = true;
+var deskSetup = ['Computer', ' Monitor', 'External webcam', 'Mic','External Keyboard', 'Mouse', 'Vibes'];
